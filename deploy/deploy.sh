631b98a26b62cfb4590a426213c3c96aa7c41460
Youssef-Guiga <78216198+Youssef-Guiga@users.noreply.github.com>
2025-04-20 16:13:44 +0100

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,5 +6,6 @@ podman stop website || true
 podman rm website || true
 
 # Run new container
-podman run -d --name website --network=host website:new
+podman run -d --name website -p 8090:80 website:new
+
 
